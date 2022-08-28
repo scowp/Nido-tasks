c84a5e1da5137a0046fa3a0acf0fb71f2842c8b9
scowp <matheusgcampo@gmail.com>
2022-08-28 20:43:23 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { CrudComponent } from './components/crud/crud.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SliderComponent,
+    CrudComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
